5177c74dcd6366e4b4c9742ee65a469da0f62fd2
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2025-11-17 12:16:57 +0100

    Commit 4

---
+++ velada-web/src/const/sponsors.ts
@@ -0,0 +1,8 @@
+import AlsaLogo from '../assets/sponsors/alsa.svg';
+
+export const sponsors: Sponsor[] = [{
+    id: 'alsa',
+    name: 'Alsa',
+    url: 'alsa.es',
+    logo: AlsaLogo
+}, /* ... otros sponsors ... */ ];
